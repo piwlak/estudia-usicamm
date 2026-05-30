13e3efa0e4cf39da42293437eabb21abc168bde4
piwlak <adrianmorales927@gmail.com>
2026-05-29 21:49:25 -0600

    fix6

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,6 @@ import "server-only";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-04-22.dahlia",
+  apiVersion: "2026-05-27.dahlia",
   typescript: true,
 });
